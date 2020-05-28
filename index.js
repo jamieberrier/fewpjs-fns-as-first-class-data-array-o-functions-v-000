5bdc9e4774fb03476bd68190ca09ed4f9ac225a1
Jamie Berrier <berrier.jamie@gmail.com>
2020-05-28 14:37:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,20 +6,25 @@ function wakeDog(dogName, dogBreed) {
 
 function leashDog(dogName, dogBreed) {
   console.log(`Leash ${dogName} the ${dogBreed}`);
+  return activity
 }
 
 function walkToPark(dogName, dogBreed) {
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+  return activity
 }
 
 function throwFrisbee(dogName, dogBreed) {
   console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  return activity
 }
 
 function walkHome(dogName, dogBreed) {
   console.log(`Walk home with ${dogName} the ${dogBreed}`);
+  return activity
 }
 
 function unleashDog(dogName, dogBreed) {
   console.log(`Unleash ${dogName} the ${dogBreed}`);
+  return activity
 }
