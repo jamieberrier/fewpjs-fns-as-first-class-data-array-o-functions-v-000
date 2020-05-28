68284797b41ab298c7c1353c18a4b436742a0f6f
Jamie Berrier <berrier.jamie@gmail.com>
2020-05-28 14:47:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashD
 
 function exerciseDog(dogName, dogBreed) {
   for (let i = 0; i < routine.length; i++) {
-
+    
   }
 }
